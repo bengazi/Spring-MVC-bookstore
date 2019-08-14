b093423b3ad0926f277d84e806ecb89e7d618949
marek.hacieja <marek.hacieja@gmail.com>
2019-08-14 20:30:32 +0200

    update books view

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,17 @@
 insert into user_role(role, description) VALUES ("ROLE_USER", "default role for user");
 
-INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "Gniewomir Sarbicki", 54.50, "Helion", "Python. Kurs dla nauczycieli i studentów", "Informatyka");
-INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "Robert C. Martin", 60.99, "Helion", "Czysty kod. Podręcznik dobrego programisty ", "Informatyka");
-INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "Lackberg Camilla", 27.99, "Czarna Owca", "Złota klatka", "kryminał");
-INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "Paris B.A.", 24.99, "Albatros", "Pozwól mi wrócić", "thriller");
-INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "Lipińska Blanka", 29.99, "Edipresse", "Ten dzień", "Literatura obyczajowa");
+INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "Gniewomir Sarbicki", 54.50, "Helion", "Python. Kurs dla nauczycieli i studentow", "Informatyka");
+INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "Robert C. Martin", 60.99, "Helion", "Czysty kod. Podrecznik dobrego programisty ", "Informatyka");
+INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "Lackberg Camilla", 27.99, "Czarna Owca", "Zlota klatka", "kryminal");
+INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "Paris B.A.", 24.99, "Albatros", "Pozwol mi wrocic", "thriller");
+INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "Lipinska Blanka", 29.99, "Edipresse", "Ten dzien", "Literatura obyczajowa");
 INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "Obama Michelle", 31.49, "Agora", "Becoming. Moja historia", "Biografia");
-INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "author4Krzysztof Gonciarz", 40.99, "KG Tofu Media", "Wybuchające Beczki - zrozumieć gry wideo", "Informatyka");
+INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "author4Krzysztof Gonciarz", 40.99, "KG Tofu Media", "Wybuchajace Beczki - zrozumiec gry wideo", "Informatyka");
 INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "J.K. Rowling", 20.99, "Media Rodzina", "Harry Potter i Komnata Tajemnic", "Fantastyka");
 INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "J.K. Rowling", 20.99, "Media Rodzina", "Harry Potter i Zakon Feniksa", "Fantastyka");
-INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "J.R.R. Tolkien", 30.99, "Amber", "Władca Pierścieni", "Fantastyka");
+INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "J.R.R. Tolkien", 30.99, "Amber", "Wladca Pierscieni", "Fantastyka");
 INSERT INTO book ( author, price, publisher, book_title, book_type) VALUES ( "J.R.R. Tolkien", 30.99, "Amber", "Silmarillion", "Fantastyka");
 
-INSERT INTO user(address,email,firstname,lastname,password,username)VALUES("adres1","email@test.pl","Marek","Testcieja","{bcrypt}$2a$10$odJqe1dJkq/dXMTfY2p.cuoNKy5OGund5ILP1gaNM9CVRf6x7Ol6y","1"); 
+INSERT INTO user(address,email,firstname,lastname,password,username)VALUES("adres1","email@test.pl","Marek","Testcieja","{bcrypt}$2a$10$odJqe1dJkq/dXMTfY2p.cuoNKy5OGund5ILP1gaNM9CVRf6x7Ol6y","1");
+
 
