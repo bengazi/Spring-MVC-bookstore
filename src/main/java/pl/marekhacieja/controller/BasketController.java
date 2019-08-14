b0a453d7b2bbec8f84c853000a17466641ade148
marek.hacieja <marek.hacieja@gmail.com>
2019-08-15 01:38:22 +0200

    refactor 'home'->'basket' and 'books'->'home'

--- src/main/java/pl/marekhacieja/controller/BasketController.java
+++ src/main/java/pl/marekhacieja/controller/BasketController.java
@@ -5,52 +5,39 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.marekhacieja.service.BookService;
 import pl.marekhacieja.service.OrderService;
 import pl.marekhacieja.service.UserService;
 
 @Controller
-public class HomeController {
-
+public class BasketController {
     private BookService bookService;
     private OrderService orderService;
     private UserService userService;
 
     @Autowired
-    public HomeController(BookService bookService, OrderService orderService, UserService userService) {
+    public BasketController(BookService bookService, OrderService orderService, UserService userService) {
         this.bookService = bookService;
         this.orderService = orderService;
         this.userService = userService;
     }
 
-    @GetMapping("/")
-    public String referToHome(Model model) {
-        bookService.addAttributeBooks(model);
-        return "home";
+    @PostMapping("/orders")
+    public String addOrder() {
+        orderService.addOrder(getLoggedUserName());
+        return "redirect:/";
     }
 
-    @GetMapping("/basket")
-    public String referToBooks(Model model) {
-        orderService.addAttributeOrder(model);
-        orderService.addAttributeSum(model);
-        return "basket";
-    }
-
-    @GetMapping("/users/{id}")
-    public String referToMyAccount(@PathVariable Long id, Model model){
-        userService.addAttributeUser(getLoggedUserName(), model);
-        return "account";
-    }
-
-    @PostMapping("/books/{id}")
-    public String addBookToOrder(@PathVariable Long id, Model model) {
-        orderService.addBookToOrder(id, model);
+    @GetMapping("/orders/{id}")
+    public String removeBook(@PathVariable Long id, Model model) {
+        orderService.deleteBookFromOrder(id, model);
         return "message";
     }
 
-     private String getLoggedUserName() {
+    private String getLoggedUserName() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String currentPrincipalName = authentication.getName();
         return currentPrincipalName;
